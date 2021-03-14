90a799eab9602d0453940159494f1dc5967665c5
kou <suhrr.code@gmail.com>
2021-03-14 18:40:34 +0900

    ビンゴカードのスタイルを調整

--- client/src/pages/room.tsx
+++ client/src/pages/room.tsx
@@ -3,10 +3,12 @@ import { useParams } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import { makeStyles } from '@material-ui/core/styles';
+import theme from '../theme';
 import Loading from '../components/loading';
 import ErrorModal from '../components/errorModal';
 import NameForm from '../components/nameForm';
@@ -17,11 +19,15 @@ const useStyles = makeStyles(() => ({
   root: {
     textAlign: 'center',
   },
+  cardRoot: {
+    maxWidth: 350,
+  },
   cardNum: {
     width: '100%',
     height: '100%',
     fontSize: '1.5em',
-    lineHeight: '150px',
+    lineHeight: '60px',
+    borderRadius: '50%',
   },
   unchecked: {
     backgroundColor: '#F2F5F7',
@@ -89,9 +95,16 @@ const Room: FC = () => {
             alignItems="center"
           >
             <Grid item xs={12}>
-              <Card>
+              <Card className={classes.cardRoot}>
+                <CardHeader
+                  title="BINGO CARD"
+                  style={{
+                    backgroundColor: theme.palette.primary.light,
+                    color: theme.palette.primary.contrastText,
+                  }}
+                />
                 <CardContent>
-                  <GridList cols={5} cellHeight={150}>
+                  <GridList cols={5} cellHeight={60}>
                     {listItems()}
                   </GridList>
                 </CardContent>
